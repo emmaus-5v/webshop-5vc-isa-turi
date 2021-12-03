88eb467a009b05b525d4611b68a89731c36d8011
Toerieeeeeee <112538@emmauscollege.nl>
2021-12-03 09:02:29 +0000

    ordenen
    Signed-off-by: Toerieeeeeee <112538@emmauscollege.nl>

--- db/create.sql
+++ db/create.sql
@@ -9,7 +9,7 @@ CREATE TABLE products (
   name VARCHAR(255),
   description TEXT,
   price NUMERIC(10, 2),
-  color_id INTEGER
+  kleur_id INTEGER
 );
 
 DROP TABLE IF EXISTS color; 
@@ -40,40 +40,37 @@ CREATE TABLE  productsCategory (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (id, name, description, code, price, color_id) values 
-(1, 'Necklace', 
+insert into products (id, name, description, code, price, kleur_id) values 
+(1, 'Necklace Gold', 
 'ketting', '816905633-0', 10.5, 1);
 
-insert into products (id, name, description, code, price, color_id) values 
-(2, 'Bracelet', 
+insert into products (id, name, description, code, price, kleur_id) values 
+(2, 'Necklace Silver', 
 'armband', '077030122-3', 11, 1);
 
-insert into products (id, name, description, code, price, color_id) values 
-(3, 'Earrings',
+insert into products (id, name, description, code, price, kleur_id) values 
+(3, 'Earrings Gold',
 'oorbellen', '445924201-X', 13.5, 1);
 
-insert into products (id, name, description, code, price, color_id) values 
-(4, 'Ring', 
+insert into products (id, name, description, code, price, kleur_id) values 
+(4, 'Earrings Silver', 
 'ring', '693155505-7', 13.5, 1);
 
-insert into products (id, name, description, code, price, color_id) values 
-(5, 'Watch band', 
+insert into products (id, name, description, code, price, kleur_id) values 
+(5, 'Ring Gold', 
 'horloge band', '686928463-6', 14, 1);
 
-insert into products (id, name, description, code, price, color_id) values 
-(6, 'Bedeltje',
+insert into products (id, name, description, code, price, kleur_id) values 
+(6, 'Ring Silver',
 'bedeltje', '492662523-7', 14, 1);
 
-insert into products (id, name, description, code, price, color_id) values 
-(7, 'Boxes', 
+insert into products (id, name, description, code, price, kleur_id) values 
+(7, 'Bracelet Gold', 
 'box', '492662523-7', 14, 1);
 
-insert into products (id, name, description, code, price, color_id) values 
-(8, 'Extra', 
+insert into products (id, name, description, code, price, kleur_id) values 
+(8, 'Bracelet Silver', 
 'extra', '492662523-7', 14, 1);
 
-insert into products (id, name, description, code, price, color_id) values 
-(9, 'Ketting zilver', 
-'ketting', '492662523-7', 14, 1);
 
 INSERT INTO color (id, name) values (1, 'Goud');
